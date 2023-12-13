cf094fd3b6f29b011f8e8ab2554c2f8e989d260e
MaliQoc <alihasanozankoc@gmail.com>
2023-12-13 22:46:16 +0300

    1. ders

---
+++ cppiletanismak/cppiletanismak.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream> // C programlama dilindeki stdio.h kütüphanesine benzer. Temel tüm komutları içerir.
+
+// Ana bir main fonksiyonu olmadan program yazamazsınız.
+int main()
+{
+    std::cout << "Hello World!\n"; // std::cout << " "; şeklinde mesajınızı tanımlayabilirsiniz. C deki printfle benzerlik göstermektedir. Zaten C++ genel olarak C ile yazıldığı için benzerlik göstermektedir.
+
+    int sayi1 = 5;
+    int sayi2 = 10; // Değişken tanımlaması C ile aynıdır.
+
+    std::cout << sayi1 + sayi2; // Örneğin iki değişkeni toplamak için ayrı bir toplama fonksiyonu oluşturmak yerine tek seferde hem topladık hem de sonucu bastırabildik.
+
+}
