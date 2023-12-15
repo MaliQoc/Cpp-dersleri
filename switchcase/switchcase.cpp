78870055e4c2ca579ad2b082c56c0c2a9629fc33
MaliQoc <alihasanozankoc@gmail.com>
2023-12-15 21:39:40 +0300

    4. ders

---
+++ switchcase/switchcase.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+int main()
+{
+    float a;
+    float b;
+    char islem;
+
+    std::cout << "Birinci sayi: " << std::endl;
+    std::cin >> a;
+    std::cout << "Ikinci sayi: " << std::endl;
+    std::cin >> b;
+
+    std::cout << "Islem giriniz: " << std::endl;
+    std::cin >> islem;
+
+    /* Switch case döngüsünde amacımız bol seçenekli programlar yazabilmektir. Örneğin hesap makinesinde yapacağınız tüm işlemleri ayrı sembollere atayıp farklı switch ağaçları olarak gösterebilirsiniz.
+    + toplama işlemine , - çıkarma işlemine götürebilir gibi. */
+
+    switch (islem) {
+    case '+':
+        std::cout << "Sonuc: " << a + b;
+        break; // Her işlemden sonra break koymanız gerekir. Yoksa diğer işlemlere geçiş yapamaz.
+    case '-':
+        std::cout << "Sonuc: " << a - b;
+        break;
+    case '*':
+        std::cout << "Sonuc: " << a * b;
+        break;
+    case '/':
+        std::cout << "Sonuc: " << a / b;
+        break;
+    default:
+        std::cout << "Hatali giris";
+        break;
+
+     /* Belirtilen seçenekler harici herhangi bir işlem girişinde default içerisindeki mesajı alırsınız. Bu nedenle switch case de default kullanmak zorundasınız.
+     Aksi takdirde yapmak istediğiniz işlem ve sonucu terminalde gözükmeyecektir. */
+
+    }
+}
