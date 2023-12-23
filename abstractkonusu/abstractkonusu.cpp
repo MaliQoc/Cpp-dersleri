94fde87ae64f90705a02caa1ed84abbac717f4ec
MaliQoc <alihasanozankoc@gmail.com>
2023-12-23 22:48:10 +0300

    12. ders

---
+++ abstractkonusu/abstractkonusu.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+
+class Hayvan {
+public:
+	virtual void sesCal() = 0;
+};
+
+//ABSTRACT (SOYUTLAMA)
+/* virtual metot sayesinde ana sınıfı soyutladık.Bu işlem seysinde istediğimiz sesi çıkarabileceğiz. Yapmasaydık sadece ana sınıftaki sesi duyardık.
+Bu şekilde gövdesiz bir metoda sahip sınıf haline geldi. Kodun düzgün çalışması için bazı durumlarda özellikle kalıtımda gereklidir. */
+
+class Kopek : public Hayvan {
+public:
+	void sesCal() {
+		std::cout << "Hav";
+	}
+};
+
+
+class Fare : public Hayvan {
+public:
+	void sesCal() {
+		std::cout << "Miu";
+	}
+};
+
+
+class Kedi : public Hayvan {
+public:
+	void sesCal() {
+		std::cout << "Miyav";
+	}
+};
+
+
+class Ordek : public Hayvan {
+public:
+	void sesCal() {
+		std::cout << "Vak";
+	}
+};
+
+
+void tumSesCal(Hayvan* ptr) {
+	ptr->sesCal();
+}
+
+
+int main()
+{
+	Kopek kopek;
+	Kedi kedi;
+	Ordek ordek;
+	Fare fare;
+
+	tumSesCal(&fare); // Sadece farenin sesini göstermek istemişiz. Ana sınıfı soyutlayarak farenin sesini yazdırır.
+}
